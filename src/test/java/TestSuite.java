be976360cc218ecb648351a1f97b279ec032a704
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2024-12-05 23:25:28 -0600

    fixed pom

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,32 @@
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Caden Finley
+ */
+public class TestSuite {
+
+    private ImageToASCII img;
+
+    private final String imagePathToVaevLogo = "vaevlogo.jpg";
+
+    @Before
+    public void setUp() {
+        img = null;
+    }
+
+    @Test
+    public void Test_Load_Image_Vaevlogo() {
+        img = new ImageToASCII(imagePathToVaevLogo);
+        assertTrue(img != null);
+    }
+
+    @Test
+    public void Test_Output_File_Exists() {
+        img = new ImageToASCII(imagePathToVaevLogo);
+        img.convertToASCIIInFile("vaevlogo.txt");
+        assertTrue(img.getOutputPath().exists());
+    }
+}
