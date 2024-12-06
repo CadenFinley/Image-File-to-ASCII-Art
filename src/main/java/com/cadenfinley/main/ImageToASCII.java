46f678a45a9456a3102aaedcd2ce516b14f0684b
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2024-12-05 18:26:48 -0600

    Revert "Update ImageToASCII.java"
    This reverts commit f8257af7e4a3e4962e6a9379a40b346ed7c94136.

--- src/main/java/com/cadenfinley/main/ImageToASCII.java
+++ src/main/java/com/cadenfinley/main/ImageToASCII.java
@@ -1,9 +1,7 @@
-package com.cadenfinley.main;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
 /**
@@ -11,34 +9,34 @@ import javax.imageio.ImageIO;
  */
 public class ImageToASCII {
 
-    private BufferedImage image;
-    private int width;
-    private int height;
-    private String imagePath;
+	private BufferedImage image;
+	private int width;
+	private int height;
+	private String imagePath;
 
-    public ImageToASCII(String pathToImage, int width, int height) {
-        image = null;
-        this.width = width;
-        this.height = height;
-        this.imagePath = pathToImage;
-        try {
-            loadImage(imagePath);
-        } catch (IOException e) {
-            System.out.println("Error loading image");
-        }
-    }
+	public ImageToASCII(String pathToImage, int width, int height) {
+		image = null;
+		this.width = width;
+		this.height = height;
+		this.imagePath = pathToImage;
+		try {
+			loadImage(imagePath);
+		} catch (IOException e) {
+			System.out.println("Error loading image");
+		}
+	}
 
-    private void loadImage(String path) throws IOException {
-        File inputImage = new File(path);
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        image = ImageIO.read(inputImage);
-    }
+	private void loadImage(String path) throws IOException {
+		File inputImage = new File(path);
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		image = ImageIO.read(inputImage);
+	}
 
-    public int getWidth() {
-        return image.getWidth();
-    }
+	public int getWidth() {
+		return image.getWidth();
+	}
 
-    public int getHeight() {
-        return image.getHeight();
-    }
+	public int getHeight() {
+		return image.getHeight();
+	}
 }
